a213c87592868c3a454127330a1adfc427189f64
Asher Antrim <aantrim246@cedarville.edu>
2024-09-05 18:44:22 -0400

    Initial Commit

---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by Asher Antrim on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
